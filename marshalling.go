f1ecdeb2a3ea8f021446e773b38c4b1037d7d232
bariteau <W@tT3hFdgbkt>
2014-10-23 12:50:25 -0700

    Added slice marshalling.

--- marshalling.go
+++ marshalling.go
@@ -19,7 +19,18 @@ func convertValue(value reflect.Value) []byte {
 		stringValue := value.Interface().(string)
 		return []byte(fmt.Sprintf("%v:%v", len([]byte(stringValue)), stringValue))
 	case reflect.Slice:
+		return convertSlice(value)
 	case reflect.Struct:
 	}
 	return []byte{}
+}
+
+func convertSlice(value reflect.Value) (representation []byte) {
+	representation = append(representation, 'l')
+	for i := 0; i < value.Len(); i++ {
+		valueRepresentation := convertValue(value.Index(i))
+		representation = append(representation, valueRepresentation...)
+	}
+	representation = append(representation, 'e')
+	return
 }
--- marshalling_test.go
+++ marshalling_test.go
@@ -1,6 +1,7 @@
 package bencode
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -13,10 +14,24 @@ func TestMarshal(t *testing.T) {
 	tests := []mtestcase{
 		mtestcase{2, "i2e"},
 		mtestcase{"fart", "4:fart"},
+		mtestcase{[]string{"fart", "butt"}, "l4:fart4:butte"},
 	}
 
 	for _, test := range tests {
 		out := string(Marshal(test.in))
+		if out != test.out {
+			t.Error("Expecting", test.out, "got", out)
+		}
+	}
+}
+
+func TestConvertSlice(t *testing.T) {
+	tests := []mtestcase{
+		mtestcase{[]int{2, 42, 666}, "li2ei42ei666ee"},
+		mtestcase{[]string{"fart", "butt"}, "l4:fart4:butte"},
+	}
+	for _, test := range tests {
+		out := string(convertSlice(reflect.ValueOf(test.in)))
 		if out != test.out {
 			t.Error("Expecting", test.out, "got", out)
 		}
